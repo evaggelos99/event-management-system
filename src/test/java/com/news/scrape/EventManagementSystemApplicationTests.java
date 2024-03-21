3b0ca0c306ee629573cf7de73c5abd0f69356bf4
Evangelos <evaggelosg99@gmail.com>
2024-03-21 17:00:42 +0200

    initial commit

---
+++ src/test/java/com/news/scrape/EventManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.scrape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
