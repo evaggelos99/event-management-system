df91b2ca72b606bab84f5a10cc44bc9f3a85be50
Evangelos Georgiou <evaggelosg99@gmail.com>
2024-08-06 23:56:38 +0300

    Remove module not versioned from parent pom

--- attendee-service/src/test/java/com/github/evaggelos99/ems/attendee/service/AttendeeServiceApplicationTest.java
+++ attendee-service/src/test/java/com/github/evaggelos99/ems/attendee/service/AttendeeServiceApplicationTest.java
@@ -1,13 +1,8 @@
 package com.github.evaggelos99.ems.attendee.service;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class AttendeeServiceApplicationTest {
-
-    @Test
-    void test() {
-
-    }
 
 }
--- organizer-service/src/test/java/com/github/evaggelos99/ems/organizer/service/OrganizerServiceApplicationTest.java
+++ organizer-service/src/test/java/com/github/evaggelos99/ems/organizer/service/OrganizerServiceApplicationTest.java
@@ -1,14 +1,13 @@
 package com.github.evaggelos99.ems.organizer.service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrganizerServiceApplicationTest {
 
-    @Test
-    void test() {
-
-    }
+//    @Test
+//    void test() {
+//
+//    }
 
 }
