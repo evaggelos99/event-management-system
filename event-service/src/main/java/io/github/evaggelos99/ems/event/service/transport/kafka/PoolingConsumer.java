d8650833061a07c7bbcc0cf5b9ab693e6b5ac47e
Evangelos Georgiou <evaggelosg99@gmail.com>
2025-04-19 21:11:55 +0300

    technical/add-event-streaming-simulator - Fix Consumer to be able to consume all partions

--- event-service/src/main/java/io/github/evaggelos99/ems/event/service/transport/kafka/PoolingConsumer.java
+++ event-service/src/main/java/io/github/evaggelos99/ems/event/service/transport/kafka/PoolingConsumer.java
@@ -1,29 +1,29 @@
 package io.github.evaggelos99.ems.event.service.transport.kafka;
 
-import com.nimbusds.oauth2.sdk.util.ListUtils;
 import io.github.evaggelos99.ems.common.api.transport.EventStreamPayload;
 import io.github.evaggelos99.ems.event.api.service.IEventMessagingService;
 import io.github.evaggelos99.ems.event.service.EventService;
 import io.github.evaggelos99.ems.kafka.lib.deserializer.ObjectDeserializer;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 @Component
-public class PoolingConsumer {
+public class PoolingConsumer implements ApplicationListener<ApplicationReadyEvent> {
 
-    public static final Duration POLL_DURATION = Duration.ofMillis(2_000);
+    public static final Duration POLL_DURATION = Duration.ofMillis(500);
     public static final int MAX_POLL_RECORDS = Integer.MAX_VALUE / 2;
     private static final Logger LOGGER = LoggerFactory.getLogger(PoolingConsumer.class);
     private final IEventMessagingService eventMessagingService;
@@ -51,31 +51,47 @@ public class PoolingConsumer {
         consumer = new KafkaConsumer<>(kafkaConsumerProperties);
     }
 
-    @Scheduled(cron = "*/10 * * * * *")
-    private void poolingMechanism() {
 
-        final Map<String, List<PartitionInfo>> ss = consumer.listTopics();
-        final List<String> list = ss.keySet()
-                .stream()
-                .filter(partitionInfos -> partitionInfos.startsWith(topicNamePrefix))
-                .toList();
+    //    @Scheduled(cron = "*/10 * * * * *")
+    @Override
+    public void onApplicationEvent(final ApplicationReadyEvent event) {
 
-        if (!subscribedTopics.equals(list)) {
-            subscribedTopics.clear();
-            subscribedTopics.addAll(list);
-            consumer.subscribe(subscribedTopics);
+        try {
+            Thread.sleep(10_000); // initial delay
+        } catch (InterruptedException ignored) {
+
         }
 
-        if (!consumer.subscription().isEmpty()) {
-            final List<EventStreamPayload> eventStreamPayloadsToBeProcessed = new LinkedList<>();
-            var records = consumer.poll(POLL_DURATION); // Poll once
-            records.forEach(record -> {
-                eventStreamPayloadsToBeProcessed.add((EventStreamPayload) objectDeserializer.convertBytesToObject(record.value()));
-            });
+        while (true) {
 
-            eventMessagingService.saveMultipleEventStreamPayload(eventStreamPayloadsToBeProcessed);
+            final List<String> listOfTopics = consumer.listTopics().keySet()
+                    .stream()
+                    .filter(partitionInfos -> partitionInfos.startsWith(topicNamePrefix))
+                    .toList();
+
+            if (!subscribedTopics.equals(listOfTopics)) {
+                subscribedTopics.clear();
+                subscribedTopics.addAll(listOfTopics);
+                consumer.assign(
+                        subscribedTopics.stream()
+                                .flatMap(topic -> consumer.partitionsFor(topic).stream()
+                                        .map(partitionInfo -> new TopicPartition(topic, partitionInfo.partition())))
+                                .toList());
+            }
+
+            if (!consumer.assignment().isEmpty()) {
+
+                final List<EventStreamPayload> eventStreamPayloadsToBeProcessed = new LinkedList<>();
+                var records = consumer.poll(POLL_DURATION);
+                records.forEach(record -> {
+                    eventStreamPayloadsToBeProcessed.add((EventStreamPayload) objectDeserializer.convertBytesToObject(record.value()));
+                });
+
+                eventMessagingService.saveMultipleEventStreamPayload(eventStreamPayloadsToBeProcessed);
+            }
+
+            consumer.commitAsync();
         }
     }
-
 }
 
